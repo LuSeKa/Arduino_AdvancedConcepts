39caf2cbdd35d71835201f56cc8e77b49a299485
Lukas Kaul <LuSeKa@users.noreply.github.com>
2019-02-13 22:41:18 +0100

    typos

--- config.h
+++ config.h
@@ -1,13 +1,13 @@
 // Hardware
 #define LED_PIN 13
 
-// Paramter initial values
+// Parameter initial values
 #define DEFAULT_BLINK_INTERVAL 200
 #define DEFAULT_PRINT_INTERVAL 10
 
 #define DEFAULT_FREQUENCY 1.0
 #define DEFAULT_AMPLITUDE 10.0
 
-// constants
+// Constants
 #define BAUDRATE 115200
 #define PRINT_ON_STARTUP true
